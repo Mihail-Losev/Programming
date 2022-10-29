b414ee24a640185095b8cd361bdca96455009087
Андрей Боев <a.boev.it@gmail.com>
2022-10-29 22:31:25 +0300

    Clear code

--- Practice 1/task 3.py
+++ Practice 1/task 3.py
@@ -1,17 +1,7 @@
 line, row_count = input().split()
 row_count = int(row_count)
-
 interval = row_count - 2
-z_count = (len(line) // (row_count + interval)) + 1
 z_len = row_count + interval
-z_diff = interval
-'''for r in range(row_count):
-    for z in range(r, z_count * row_count + 1, row_count + interval):
-        print(line[z], end='')
-        if 0 < r < row_count - 1:
-            print(line[z + z_diff], end='')
-    z_diff -= 1'''
-
 matr = [line[i:i+z_len] for i in range(0, len(line), z_len)]
 matr[-1] += ' ' * (z_len - len(matr[-1]))
 s_res = ''
